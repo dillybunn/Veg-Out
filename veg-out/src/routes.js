fe270c0192e5a16e950d13f345e4cf2c36c3881e
Billy Dunn <bdunn27@gmail.com>
2024-04-15 12:11:50 -0400

    route and error

---
+++ veg-out/src/routes.js
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+];
+
+export default routes;
